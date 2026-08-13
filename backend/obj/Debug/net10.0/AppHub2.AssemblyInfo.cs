b6076b5d987f082615d4d466c184d11e41e2e010
Rd27p <g.a.raka.darma.p.k@gmail.com>
2026-08-13 15:55:48 +0700

    fix: [GAR] Perbaikan pada migrasi database dan penambahan model baru

--- backend/obj/Debug/net10.0/AppHub2.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/AppHub2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppHub2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceeeb7766bbf853e17fa6f198b72e92e3ccce948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfac5e4094de7149fcbe650d765193ae3e1b9ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppHub2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppHub2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
