ec72faa26a222164fc7d72df9471e6848109b4d8
Rd27p <g.a.raka.darma.p.k@gmail.com>
2026-08-20 16:34:37 +0700

    Add new backend folder from branch backend

--- backend/obj/Debug/net10.0/AppHub2.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/AppHub2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppHub2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45bcbcf07ffc38a327b73dcf9bfca2181d157cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c00e7e98c289fe684a96b7839cb06b1f3abc45b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppHub2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppHub2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
